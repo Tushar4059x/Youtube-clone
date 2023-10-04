2d7e27c51f5bbf917d1bfcb68e05f269d800a693
Tushar4059x <gulyanitushar@gmail.com>
2023-10-04 09:46:57 +0530

    1st commit- next js-installed

---
+++ youtube-clone/src/components/VideoCard.tsx
@@ -0,0 +1,25 @@
+export function VideoCard() {
+    return <div>
+
+        <img src="thumbnail1.jpeg" alt="" />
+        <div className="grid - grid-cols-12">
+            <div className=""col-span-2>
+                <img className = {"rounded-full w-20 h-20"} src="thumbnail1.jpeg" alt="" />
+            </div>
+                <div className="col-span-10" 
+                pl-5>
+                lufyyyyyyyyyy|
+
+                yyyyy
+                <div className="col-span-11
+                pl-5 text-grey-600 text-base">
+                    one piece
+                <div className="col-span-11 pl-5
+                text-grey-600 text-base">
+                    auehds
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+}
